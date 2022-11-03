eaf49db12454544ae11cbc873789119ac2d46eb2
hasibulHossain <hasibulhossain52@gmail.com>
2022-11-03 19:29:05 +0600

    add: added header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+const colors = require('tailwindcss/colors');
 const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -6,9 +7,38 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+    },
+    colors: {
+      primary: {
+        100: '#a273ff',
+        200: '#935bff',
+        300: '#8344ff',
+        400: '#742cff',
+        500: '#6415FF',
+        600: '#5a13e6',
+        700: '#5011cc',
+        800: '#460fb3',
+        900: '#3c0d99',
+      },
+
+      secondary: {
+        100: '#7c8ba1',
+        200: '#667892',
+        300: '#506582',
+        400: '#3a5173',
+        500: '#243E63',
+        600: '#203859',
+        700: '#1d324f',
+        800: '#192b45',
+        900: '#16253b',
+      },
+      ...colors,
+    },
     extend: {
       fontFamily: {
-        'sans': ['Manrope, Noto Serif Bengali, sans-serif', ...defaultTheme.fontFamily.sans]
+        'sans': ['Inter, sans-serif', ...defaultTheme.fontFamily.sans]
       }
     },
   },
