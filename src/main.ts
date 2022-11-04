d33ac05a8422c8d469053e1c13eb5797a473f115
hasibulHossain <hasibulhossain52@gmail.com>
2022-11-04 19:19:13 +0600

    add: added price and feature section

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import 'flowbite';
 import i18next from 'i18next';
 import languageDetector from 'i18next-browser-languagedetector';
 import httpApi from 'i18next-http-backend';
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     container: {
@@ -42,5 +43,5 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
 }
